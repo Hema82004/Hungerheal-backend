c3d28d9812c8c404fa854661d0e25c7be8149f86
Hema82004 <163748243+Hema82004@users.noreply.github.com>
2025-09-15 15:30:25 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "HUNGERHEAL",
-  password: "postgres",
-  port: 5432,
+  connectionString: process.env.postgresql://hungerhealdb_user:x43WJj2RpF8CEuuujU6vpViqq6HHEF6U@dpg-d33u5sfdiees739sftdg-a/hungerhealdb,  // ✅ use Render’s DATABASE_URL
+  ssl: {
+    rejectUnauthorized: false,  // Render requires SSL
+  },
 });
 
 module.exports = pool;
+
